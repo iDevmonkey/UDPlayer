0400132708f2591b56acdcc033bae2e5a2e6f5b9
iDevmonkey <houxiongyao@gmail.com>
2020-05-17 20:36:53 +0800

    Initial Commit

---
+++ UDPlayer/UDPlayer.h
@@ -0,0 +1,13 @@
+//
+//  UDPlayer.h
+//  UDPlayer
+//
+//  Created by Mac on 2020/5/17.
+//  Copyright © 2020 com.hzhihui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UDPlayer : NSObject
+
+@end
---
+++ UDPlayer/UDPlayer.m
@@ -0,0 +1,13 @@
+//
+//  UDPlayer.m
+//  UDPlayer
+//
+//  Created by Mac on 2020/5/17.
+//  Copyright © 2020 com.hzhihui. All rights reserved.
+//
+
+#import "UDPlayer.h"
+
+@implementation UDPlayer
+
+@end
